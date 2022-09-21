85d6b80ce5cdc27cc53530915796477b60bcd4d0
jingchen <jingchen@student.42madrid.com>
2022-09-21 19:25:27 +0200

    20220921

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -1,38 +1,37 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jingchen <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/09/19 18:02:36 by jingchen          #+#    #+#             */
-/*   Updated: 2022/09/19 18:53:54 by jingchen         ###   ########.fr       */
+/*   Created: 2022/09/21 17:55:09 by jingchen          #+#    #+#             */
+/*   Updated: 2022/09/21 18:50:35 by jingchen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memchr(const void *s, int c, size_t n)
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
 	size_t	i;
 
 	i = 0;
-	if (!s)
-		return(0);
 	while (i < n)
 	{
-		if (*(unsigned int *)(s + i)== (unsigned int)c)
-			return ((unsigned int *)(s + i));
-		i+=4;
+		if (*(char *)(s1 + i) != *(char *)(s2 + i))
+			return (*(char *)(s1 + i) - *(char *)(s2 + i));
+		i++;
 	}
 	return (0);
 }
-
-int main ()
+/*int main()
 {
-	int s[] = {1,2,3,5,8,7,5,5,4,7,7,8,5,5};
-	int c = 2;
-	int *a = ft_memchr(s, c, 12);
-	printf("%i\n", a[2]);
-			return 0;
-}
+	int	s1[] = {1, 2, 7};
+	int	s2[] = {1, 2, 3};
+
+	printf("%d\n", ft_memcmp(s1, s2, 12));
+	printf("%d\n", memcmp(s1, s2, 12));
+
+	return 0;
+}*/
